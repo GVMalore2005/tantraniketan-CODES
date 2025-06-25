b78377d2c501f7f850d2d29e595b6f7fd0bf3f3c
GVMalore2005 <maloregaurav162@gmail.com>
2025-06-25 11:47:49 +0530

    file.py

---
+++ file.py
@@ -0,0 +1,9 @@
+# smart.py
+
+# Ask the user for their name and age, then print a message
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+2
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Hello, {name}! You are {age} years old.")
